51824cd6c341eb26bb3da88b36e4dde549c39007
Mohit <mohit10209@gmail.com>
2025-03-16 17:17:35 +0530

    Allowed CORS

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ import 'express-async-errors';
 import EventEmitter from 'events';
 import express from 'express';
 import http from 'http';
+import cors from 'cors';
 import { Server as socketIo } from 'socket.io'; 
 import connectDB from '../config/connect.js';
 import notFoundMiddleware from '../middleware/not-found.js';
@@ -27,8 +28,14 @@ dotenv.config();
 EventEmitter.defaultMaxListeners = 20;
 
 const app = express();
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
-// app.use(cors());
 
 const server = http.createServer(app);
 
